3037ee95c5e876a1503232133a683a887ddf19d1
Stageddat <chmarc72@gmail.com>
2025-06-03 18:24:10 +0200

    feat: change default port from 80 to 7777

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-  PORT: process.env.PORT || 80,
+  PORT: process.env.PORT || 7777,
   MASTER_PASSWORD: process.env.MASTER_PASSWORD,
   SPECIAL_PASSWORD: process.env.SPECIAL_PASSWORD,
   TEMP_DIR: "temp/",
